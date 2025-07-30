373f11d62c858495470ebeb8fbbacb04f962bdd0
류정균 <prank01@naver.com>
2025-07-30 22:07:06 +0900

    spring start 0730

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,21 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000; //1000원 할인
+
+
+
+    @Override
+    public int discount(Member member, int price) {
+        //enum에서는 == 처리를 해야한다.
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
